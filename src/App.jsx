9590f0a7a54710d9323a95420f855d174d97db40
Dhanesh Saini <sainidhanesh58@gmail.com>
2024-06-27 13:15:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <SearchBar />
-            <main className="bg-[url(bg.jpg)] bg-cover bg-center md:min-h-screen md:max-h-screen md:overflow-hidden w-full md:fixed">
+            <main className="bg-[url('/bg.jpg')] bg-cover bg-center md:min-h-screen md:max-h-screen md:overflow-hidden w-full md:fixed">
                 <div className="flex flex-col md:flex-row w-full">
                     <section className="flex-1">
                         <FindePincode />
